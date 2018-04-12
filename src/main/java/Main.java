84206992cfc17e70c6c1f7d20f0b135a7f5ef9ca
haizing <779397435@qq.com>
2018-04-12 16:54:25 +0800

    0412 修改

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello Maven");
         System.out.println("Hello Maven -- by test");//修改
         System.out.println("Hello Maven -- by test2");//添加一句代码
+        System.out.println("Hello Maven -- by test0412");//添加一句代码
     }
 }
